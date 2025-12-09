e64c2cdab233a0c13a493b6a4c81e3dd79ebacff
Indra Ardika <indraardika@gmail.com>
2025-12-09 12:44:24 +0800

    baru pendidikan

--- resources/views/kepegawaian/pendidikan.blade.php
+++ resources/views/kepegawaian/pendidikan.blade.php
@@ -84,6 +84,9 @@
                                             <option value="SD">SD</option>
                                             <option value="SMP">SMP</option>
                                             <option value="SMA">SMA</option>
+                                            <option value="SMK">SMK</option>
+                                            <option value="D1">D1</option>
+                                            <option value="D2">D2</option>
                                             <option value="D3">D3</option>
                                             <option value="D4">D4</option>
                                             <option value="S1">S1</option>
