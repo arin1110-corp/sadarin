1db12ae99570fd5197e4147a75acbf21e3421db4
Indra Ardika <indraardika@gmail.com>
2026-01-12 14:10:31 +0800

    asdsa

--- resources/views/homepage_detailpegawai.blade.php
+++ resources/views/homepage_detailpegawai.blade.php
@@ -899,8 +899,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
 
 
-<!-- jQuery (wajib untuk Select2) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <!-- Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
