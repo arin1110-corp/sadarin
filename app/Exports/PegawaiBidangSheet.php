afea4968f308d8d09fdfc5bbab42463f155624a9
Indra Ardika <indraardika@gmail.com>
2026-01-20 13:35:27 +0800

    EXCEL BARU

--- app/Exports/PegawaiBidangSheet.php
+++ app/Exports/PegawaiBidangSheet.php
@@ -42,7 +42,7 @@ class PegawaiBidangSheet implements FromCollection, WithHeadings, WithStyles, Wi
                     'sadarin_golongan.golongan_nama as golongan',
 
             DB::raw("CASE
-                WHEN sadarin_user.user_tmt = '1990-01-01' THEN 'Belum Melakukan'
+                WHEN sadarin_user.user_tmt = '1990-01-01' THEN sadarin_user.user_tmt
                 ELSE 'Sudah'
             END as status_pemuktahiran"),
 
@@ -55,6 +55,35 @@ class PegawaiBidangSheet implements FromCollection, WithHeadings, WithStyles, Wi
                 WHEN sadarin_user.user_jabatan = '65' THEN 'Jabatan masih PPPK'
                 ELSE 'Sudah'
             END as status_jabatan"),
+
+            DB::raw("CASE WHEN sadarin_user.user_tgllahir='1990-01-01' then sadarin_user.user_tgllahir
+            else 'Sudah'
+            END as tanggal_lahir"),
+
+            DB::raw("CASE
+                WHEN sadarin_user.user_pendidikan = '0' THEN 'Belum Update'
+                ELSE 'Sudah'
+            END as pendidikan"),
+
+            DB::raw("CASE
+                WHEN sadarin_user.user_norek = '-' THEN 'Belum Update'
+                ELSE 'Sudah'
+            END as nomor_rekening"),
+
+            DB::raw("CASE
+                WHEN sadarin_user.user_npwp = '-' THEN 'Belum Update'
+                ELSE 'Sudah'
+            END as npwp"),
+
+            DB::raw("CASE
+                WHEN sadarin_user.user_notelp = '-' THEN 'Belum Update'
+                ELSE 'Sudah'
+            END as nomor_hp"),
+
+            DB::raw("CASE
+                WHEN sadarin_user.user_email = '-' THEN 'Belum Update'
+                ELSE 'Sudah'
+            END as email"),
             )
 
             ->leftJoin('sadarin_bidang', 'sadarin_user.user_bidang', '=', 'sadarin_bidang.bidang_id')
@@ -70,7 +99,7 @@ class PegawaiBidangSheet implements FromCollection, WithHeadings, WithStyles, Wi
 
     public function headings(): array
     {
-        return ['NIP', 'NIK', 'Nama', 'Jenis Kerja', 'Bidang', 'Jabatan', 'Golongan', 'Status Pemuktahiran', 'Status Foto', 'Status Jabatan'];
+        return ['NIP', 'NIK', 'Nama', 'Jenis Kerja', 'Bidang', 'Jabatan', 'Golongan', 'Status TMT', 'Status Foto', 'Status Jabatan', 'Tanggal Lahir', 'Pendidikan', 'Nomor Rekening', 'NPWP', 'Nomor HP', 'Email'];
     }
 
     public function title(): string
@@ -104,14 +133,14 @@ class PegawaiBidangSheet implements FromCollection, WithHeadings, WithStyles, Wi
                 ->getFill()
                 ->setFillType(Fill::FILL_SOLID)
                 ->getStartColor()
-                ->setRGB($row->jabatan === 'PPPK' ? 'FFCC99' : 'FFFFFF');
+                ->setRGB($row->jabatan === 'PPPK' ? 'FF0000' : 'FFFFFF');
             // H - Pemuktahiran
             $sheet
                 ->getStyle("H{$rowIndex}")
                 ->getFill()
                 ->setFillType(Fill::FILL_SOLID)
                 ->getStartColor()
-                ->setRGB($row->status_pemuktahiran === 'Sudah' ? 'CCFFCC' : 'FF9999');
+                ->setRGB($row->status_pemuktahiran === '1990-01-01' ? 'FF0000' : 'CCFFCC');
 
             // I - Foto
             $sheet
@@ -127,25 +156,70 @@ class PegawaiBidangSheet implements FromCollection, WithHeadings, WithStyles, Wi
                 ->getFill()
                 ->setFillType(Fill::FILL_SOLID)
                 ->getStartColor()
-                ->setRGB($row->status_jabatan === 'Sudah' ? 'CCFFCC' : 'FF9999');
+                ->setRGB($row->status_jabatan === 'Sudah' ? 'CCFFCC' : 'FF0000');
+
+            // K ===== TANGGAL LHR =====
+            $sheet
+                ->getStyle("K{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->tanggal_lahir === '1990-01-01' ? 'FF0000' : 'CCFFCC');
+
+            // L ===== Pendidikan =====
+            $sheet
+                ->getStyle("L{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->pendidikan === 'Belum Update' ? 'FF0000' : 'CCFFCC');
+            // M ===== Nomor Rekening =====
+            $sheet
+                ->getStyle("M{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->nomor_rekening === 'Belum Update' ? 'FF0000' : 'CCFFCC');
+            // N ===== NPWP =====
+            $sheet
+                ->getStyle("N{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->npwp === 'Belum Update' ? 'FF0000' : 'CCFFCC');
+            // O ===== Nomor HP =====
+            $sheet
+                ->getStyle("O{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->nomor_hp === 'Belum Update' ? 'FF0000' : 'CCFFCC');
+            // P ===== Email =====
+            $sheet
+                ->getStyle("P{$rowIndex}")
+                ->getFill()
+                ->setFillType(Fill::FILL_SOLID)
+                ->getStartColor()
+                ->setRGB($row->email === 'Belum Update' ? 'FF0000' : 'CCFFCC');
+
 
             $rowIndex++;
         }
 
         // ===== HEADER =====
-        $sheet->getStyle('A1:I1')->getFont()->setBold(true);
-        $sheet->getStyle('A1:I1')->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setRGB('DDDDDD');
+        $sheet->getStyle('A1:P1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:P1')->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setRGB('DDDDDD');
 
         // ===== BORDER =====
         $sheet
-            ->getStyle("A1:I{$lastRow}")
+            ->getStyle("A1:P{$lastRow}")
             ->getBorders()
             ->getAllBorders()
             ->setBorderStyle(Border::BORDER_THIN)
             ->setColor(new Color('000000'));
 
         // ===== AUTOSIZE =====
-        foreach (range('A', 'I') as $col) {
+        foreach (range('A', 'P') as $col) {
             $sheet->getColumnDimension($col)->setAutoSize(true);
         }
 
